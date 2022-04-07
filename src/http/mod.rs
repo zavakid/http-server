12e52a064e4c4e7823937b5d1515c3aeade9faf1
zavakid <zava.kid@gmail.com>
2022-04-07 23:12:36 +0800

    feat: add response model

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -1,8 +1,12 @@
+pub use method::Method;
+pub use query_string::{QueryString, Value as QueryStringValue};
+pub use request::ParseError;
+pub use request::Request;
+pub use response::Response;
+pub use status_code::StatusCode;
+
 mod request;
 mod method;
 mod query_string;
-
-pub use request::Request;
-pub use request::ParseError;
-pub use method::Method;
-pub use query_string::{QueryString, Value as QueryStringValue};
+mod response;
+mod status_code;
