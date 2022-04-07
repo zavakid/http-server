7f7ce0977a83ec69260290f00562d3b7abeeeaf9
zavakid <zava.kid@gmail.com>
2022-04-07 13:24:04 +0800

    feat: parse request

--- src/http/request.rs
+++ src/http/request.rs
@@ -4,7 +4,9 @@ use std::fmt::{Debug, Display, Formatter};
 use std::fmt::Result as FmtResult;
 use std::str;
 use std::str::Utf8Error;
-use crate::http::ParseError::InvalidEncoding;
+
+use crate::http::method::MethodError;
+use crate::http::ParseError::{InvalidEncoding, InvalidMethod, InvalidProtocol, InvalidRequest};
 
 use super::method::Method;
 
@@ -23,11 +25,41 @@ impl Request {
 impl TryFrom<&[u8]> for Request {
     type Error = ParseError;
 
+    //e.g. GET /search?name=abc&sort=1 HTTP/1.1\r\n..HEADERS...
     fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
         let request = str::from_utf8(buf)?;
 
-        todo!()
+        let (method, request) = get_next_word(request).ok_or(InvalidRequest)?;
+        let (mut path, request) = get_next_word(request).ok_or(InvalidRequest)?;
+        let (protocol, _) = get_next_word(request).ok_or(InvalidRequest)?;
+
+        if protocol != "HTTP/1.1" {
+            return Err(InvalidProtocol);
+        }
+
+        let method: Method = method.parse()?;
+
+        let mut query_string = None;
+        if let Some(i) = path.find('?') {
+            query_string = Some(path[i + 1..].to_string());
+            path = &path[..i];
+        }
+
+        Ok(Self {
+            path: path.to_string(),
+            query_string ,
+            method,
+        })
     }
+}
+
+fn get_next_word(request: &str) -> Option<(&str, &str)> {
+    for (i, c) in request.chars().enumerate() {
+        if c == ' ' || c == '\r' {
+            return Some((&request[..i], &request[i + 1..]));
+        }
+    }
+    None
 }
 
 pub enum ParseError {
@@ -45,6 +77,12 @@ impl ParseError {
             Self::InvalidProtocol => "Invalid Protocol",
             Self::InvalidMethod => "Invalid Method",
         }
+    }
+}
+
+impl From<MethodError> for ParseError {
+    fn from(_: MethodError) -> Self {
+        InvalidMethod
     }
 }
 
