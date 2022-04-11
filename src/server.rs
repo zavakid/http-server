e32d563625faa7420d55293ad9bcd0fefcbd309f
zavakid <zava.kid@gmail.com>
2022-04-11 13:24:36 +0800

    refactor: extract handler_connection

--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,5 @@
 use std::io::Read;
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
 use std::sync::Arc;
 use std::thread::spawn;
 
@@ -30,32 +30,33 @@ impl Server {
 
         loop {
             match listener.accept() {
-                Ok((mut stream, _addr)) => {
-                    let handler_for_request = handler.clone();
-                    spawn(move || {
-                        let mut buffer = [0; 1024];
-                        match stream.read(&mut buffer) {
-                            Ok(_) => {
-                                log::info!(
-                                    "Received a request: {}",
-                                    String::from_utf8_lossy(&buffer)
-                                );
-
-                                let response = match Request::try_from(&buffer[..]) {
-                                    Ok(request) => handler_for_request.handle_request(&request),
-                                    Err(e) => handler_for_request.handle_bad_request(&e),
-                                };
-
-                                if let Err(e) = response.send(&mut stream) {
-                                    log::error!("Failed to send response: {}", e);
-                                }
-                            }
-                            Err(e) => log::error!("Failed to read from connection: {}", e),
-                        }
-                    });
+                Ok((stream, _addr)) => {
+                    handle_connection(stream, Arc::clone(&handler));
                 }
                 Err(e) => log::error!("Failed to establish a connection: {}", e),
             }
         }
     }
+}
+
+fn handle_connection(mut stream: TcpStream, handler: Arc<dyn Handler>) {
+    let handler_for_request = handler.clone();
+    spawn(move || {
+        let mut buffer = [0; 1024];
+        match stream.read(&mut buffer) {
+            Ok(_) => {
+                log::info!("Received a request: {}", String::from_utf8_lossy(&buffer));
+
+                let response = match Request::try_from(&buffer[..]) {
+                    Ok(request) => handler_for_request.handle_request(&request),
+                    Err(e) => handler_for_request.handle_bad_request(&e),
+                };
+
+                if let Err(e) = response.send(&mut stream) {
+                    log::error!("Failed to send response: {}", e);
+                }
+            }
+            Err(e) => log::error!("Failed to read from connection: {}", e),
+        }
+    });
 }
